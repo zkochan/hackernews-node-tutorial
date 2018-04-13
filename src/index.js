8308be787ace9ac1fd5239018a2601e49996ff2a
Zoltan Kochan <zoltan.kochan@gmail.com>
2018-04-13 19:00:36 +0300

    step 2

--- src/index.js
+++ src/index.js
@@ -1,41 +1,14 @@
 const { Prisma } = require('prisma-binding')
 const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
 
 // 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.db.query.links({}, info)
-    },
-    link: (root, args) => links.find((link) => link.id === args.id),
-  },
-  Mutation: {
-    post: (root, args, context, info) => {
-      return context.db.mutation.createLink({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      }, info)
-    },
-    updateLink: (root, args) => {
-      const link = links.find((link) => link.id === args.id)
-      if (typeof args.url === 'string') {
-        link.url = args.url
-      }
-      if (typeof args.description === 'string') {
-        link.description = args.description
-      }
-      return link
-    },
-    deleteLink: (root, args) => {
-      const linkIndex = links.findIndex((link) => link.id === args.id)
-      const link = links[linkIndex]
-      links.splice(linkIndex, linkIndex + 1)
-      return link
-    },
-  },
+  Query,
+  Mutation,
+  AuthPayload
 }
 
 // 3
